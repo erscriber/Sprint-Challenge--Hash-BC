dc8aa047e5daa6ee440b6070adebaf667dba5e00
erscriber <erscriber@gmail.com>
2019-10-21 14:57:51 -0400

    Hashtable exercises complete

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,17 +1,21 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
                         hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+                        hash_table_retrieve)
 
 
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
-    """
-    YOUR CODE HERE
-    """
+    # To print ht
+    for index, item in enumerate(weights):
+        match = hash_table_retrieve(ht, limit-item)
+        if match is not None:
+            if index > match:
+                return(index, match)
+            return(match, index)
+        else:
+            hash_table_insert(ht, item, index)
 
     return None
 
